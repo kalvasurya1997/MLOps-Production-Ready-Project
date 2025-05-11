aabe2d36bce37d06e33fbb3c2e6aa480b563791b
surya Kalva <kalvasurya1997@gmail.com>
2025-05-10 23:43:36 -0400

    env setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Surya",
+    author_email="kalvasurya1997@gmail.com",
+    packages=find_packages()
+)
